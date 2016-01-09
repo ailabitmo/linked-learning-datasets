b37e6969c83f186f1a70d9e5e9bc2b58306d94f1
Aleksei Romanov <Gloomspb@gmail.com>
2016-01-09 23:05:25 +0300

    fix

--- edx/course.php
+++ edx/course.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Aggregation plugin for Open edX</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
@@ -265,8 +265,5 @@ fclose($fileopen);
 
 printf('Script time %.4F seconds.', $time);
 ?>
-
-
-
 </body>
 </html>
